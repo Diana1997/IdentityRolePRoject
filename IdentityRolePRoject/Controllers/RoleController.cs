f3b045a353fee6fb435debe5eb176f25a37ae4ca
Diana <Diana@DESKTOP-RLN4LA8>
2018-03-29 18:47:56 +0400

    ---

---
+++ IdentityRolePRoject/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using IdentityRolePRoject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityRolePRoject.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            return View(db.Roles.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind(Include = "Id, Name")]RoleViewModel roleViewModel)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole(roleViewModel.Name);
+                var roleresult = await roleManager.CreateAsync(role);
+            }
+            return View(roleViewModel);
+        }
+    }
+}
